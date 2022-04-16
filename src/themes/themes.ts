e99498d19f365600e3db7b6f091dab88ee9a1dbd
launchpad5682 <launchpad5682@gmail.com>
2022-04-16 16:38:21 +0530

    feat: Themes added

---
+++ src/themes/themes.ts
@@ -0,0 +1,17 @@
+export const darkTheme = {
+  backgroundColor: "145deg, #1e1928, #2a253a",
+  darkColor: "#1e1928",
+  textColor: "#9290cc",
+  shadowColorOne: "#171420",
+  shadowColorTwo: "#312a42",
+  boxShadow: "30px 30px 58px #171420,-30px -30px 58px #312a42",
+};
+
+export const lightTheme = {
+  backgroundColor: "145deg, #7473b0, #a09ff4",
+  darkColor: "#7473b0",
+  textColor: "#3d3c5a",
+  shadowColorOne: "#595887",
+  shadowColorTwo: "#bbbaff",
+  boxShadow: "10px 10px 58px #595887,-10px -10px 58px #bbbaff",
+};
