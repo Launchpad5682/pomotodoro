e1a5a023c267d9bcffffdb3a370ad27d6cec9954
launchpad5682 <launchpad5682@gmail.com>
2022-04-17 16:14:03 +0530

    feat: Theme Provider added with default themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { useDataProvider } from "./context/data-context";
+import { Home } from "./routes/Home";
+import { Todo } from "./routes/Todo";
 
 function App() {
+  const { theme } = useDataProvider();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
