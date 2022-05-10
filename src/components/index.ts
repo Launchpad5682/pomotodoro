238826c41678cb5baaed41444d35f9ef1bb8b047
launchpad5682 <launchpad5682@gmail.com>
2022-05-10 12:57:30 +0530

    feat: Index file for components added

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { CheckBox } from "./CheckBox";
+export { FlatButton } from "./FlatButton";
+export { FloatingActionButton } from "./FloatingActionButton";
+export { Header } from "./Header";
+export { ModalForm } from "./ModalForm";
+export { Timer } from "./Timer";
+export { SearchBar } from "./SearchBar";
+export { SideBar } from "./SideBar";
+export { TodoList } from "./TodoList";
+export { TodoListItem } from "./TodoListItem";
