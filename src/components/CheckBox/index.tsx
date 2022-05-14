4c54fa7bd6dabba831433224076b565831d50d52
launchpad5682 <launchpad5682@gmail.com>
2022-05-14 21:21:08 +0530

    feat: Checkbox component added

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -1,13 +1,20 @@
+import { ChangeEventHandler } from "react";
 import * as Styled from "./styles";
 
 type Props = {
   title: string;
+  checked: boolean;
+  changeHandler: ChangeEventHandler<HTMLInputElement>;
 };
 
-export function CheckBox({ title }: Props) {
+export function CheckBox({ title, checked, changeHandler }: Props) {
   return (
     <Styled.Label>
-      <Styled.CheckBox />
+      <Styled.CheckBox
+        checked={checked}
+        onChange={changeHandler}
+        name="completed"
+      />
       <Styled.LabelText>{title}</Styled.LabelText>
     </Styled.Label>
   );
