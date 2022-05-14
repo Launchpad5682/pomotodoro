822dfcca85317a8b7aa4cb05c2e1ea7c45e9aeb0
launchpad5682 <launchpad5682@gmail.com>
2022-05-14 21:24:18 +0530

    feat: TodoList added

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,10 +1,13 @@
 import * as Styled from "./styles";
 import { TodoListItem } from "../";
+import { useDataProvider } from "../../context/data-context";
 
 export function TodoList() {
+  const { tasks } = useDataProvider();
   return (
     <Styled.TodoList>
-      <TodoListItem title="Hello" />
+      {tasks.length > 0 &&
+        tasks.map((task) => <TodoListItem task={task} key={task._id} />)}
     </Styled.TodoList>
   );
 }
