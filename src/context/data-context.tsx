c3c01e76a94a0321afed298d2206e9b790d66380
launchpad5682 <launchpad5682@gmail.com>
2022-05-14 21:25:14 +0530

    refactor: updated data-context initial values

--- src/context/data-context.tsx
+++ src/context/data-context.tsx
@@ -1,35 +1,34 @@
 import { createContext, ReactNode, useContext, useReducer } from "react";
+import { TaskInterface } from "../components/types";
 import { reducer } from "../reducers/reducer";
 import { lightTheme } from "../themes/themes";
 
-export interface TaskInterface {}
-
 interface DataContextInterface {
   theme: any;
-  modal: boolean;
+  modal: { visible: boolean; edit: boolean };
   dispatch: Function;
-  task: null | TaskInterface;
-  tasks: [] | TaskInterface[];
+  tasks: TaskInterface[] | [];
+  activeTask: null | TaskInterface;
 }
 
 const initialState = {
   theme: lightTheme,
-  modal: false,
-  task: null,
+  modal: { visible: false, edit: false },
   tasks: [],
+  activeTask: null,
   dispatch: () => {},
 };
 
 const DataContext = createContext<DataContextInterface>(initialState);
 
 const DataProvider = ({ children }: { children: ReactNode }) => {
-  const [{ theme, modal, task, tasks }, dispatch] = useReducer(
+  const [{ theme, modal, activeTask, tasks }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   return (
-    <DataContext.Provider value={{ theme, modal, task, tasks, dispatch }}>
+    <DataContext.Provider value={{ theme, modal, activeTask, tasks, dispatch }}>
       {children}
     </DataContext.Provider>
   );
